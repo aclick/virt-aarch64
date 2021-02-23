382e3e16e2b4e6bbb199d55c2cef835456b095f2
Austin Click <austin.click@gmail.com>
2021-02-22 18:43:45 -0600

    Update run script flags

--- run.sh
+++ run.sh
@@ -2,9 +2,10 @@
 
 qemu-system-aarch64 \
     -M virt -m 1024 -cpu cortex-a57 \
-    -kernel ArchLinuxARM-rpi-aarch64-latest/boot/Image \
-    -initrd ArchLinuxARM-rpi-aarch64-latest/boot/initramfs-linux.img \
+    -kernel Image \
+    -initrd initramfs-linux.img \
     -no-reboot \
-    -serial stdio \
-    -drive file=archpi.img,if=virtio \
-    -append "root=/dev/vda rw console=ttyAMA0"
+    -nographic \
+    -serial mon:stdio \
+    -drive file=raw.img,format=raw,if=virtio \
+    -append "root=/dev/vda2 rw console=ttyAMA0"
